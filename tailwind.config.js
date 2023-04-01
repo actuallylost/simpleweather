c1d1fc2219eba2b02121c22100bd7ba298e8146c
Argyris <arkatsy@gmail.com>
2023-04-01 14:41:10 +0300

    add font, metatags, global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-comfortaa)", "sans-serif"],
+            },
+        },
     },
     plugins: [
         require("@tailwindcss/forms"),
